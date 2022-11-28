ec7ce9e659e45f558e729f5857d660a0fa4b1607
Tenable.cs <admin@accurics.com>
2022-11-28 12:47:38 -0800

    Auto Remediation By Tenable.cs

--- main.tf
+++ main.tf
@@ -162,3 +162,27 @@ resource "aws_s3_bucket_policy" "awsjamdemo_bucketpolicy" {
 }
 POLICY
 }
+resource "aws_s3_bucket_policy" "awsjamdemo_bucketpolicy" {
+  bucket = aws_s3_bucket.awsjamdemo_bucket.id
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "awsjamdemo_bucket-restrict-access-to-users-or-roles",
+      "Effect": "Allow",
+      "Principal": [
+        {
+          "AWS": [
+            "<aws_policy_role_arn>"
+          ]
+        }
+      ],
+      "Action": "s3:GetObject",
+      "Resource": "arn:aws:s3:::awsjamdemo_bucket/*"
+    }
+  ]
+}
+POLICY
+}
