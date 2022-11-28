9148e679ea7bfa7221f6592adef938e5efe84eea
Tenable.cs <admin@accurics.com>
2022-11-28 12:45:17 -0800

    Auto Remediation By Tenable.cs

--- main.tf
+++ main.tf
@@ -116,7 +116,7 @@ resource "aws_security_group" "prod_web" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["<cidr>"]
     //    cidr_blocks = ["172.31.0.0/28"]
   }
   ingress {
